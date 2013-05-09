dd62a937cee110fc960aad840fdfe282eee20db6
Rahul Kumar <rahulbenegal@gmail.com>
2013-05-09 11:58:31 +0530

    some changes

--- progs/view.rb
+++ progs/view.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
 
+# DESC: View data of movies table.
+#
+# - edit values and save in table - currently only screen shows edit
+# - reload data on some key
+# - view plot and intro on some key
+# - show imdbtop 100 on a key in a sepa window that can be reused.
+# - when viewing wiki info be able to backtab to history
 # Using tablewidget with sqlite3 resultset
 # TODO : make columns hidden on key - toggle, how to get back then
 # TODO : move column position
@@ -13,8 +20,11 @@ require 'kubrick'
 require 'kubrick/commons'
 
 #
-$table = "movies_back"
+$table = "movie"
 
+      BESTPIC = 6
+      BESTFOR = 10
+      CANNES = 11
 def get_db
   dbname = "movie.sqlite"
   raise unless File.exists? dbname
@@ -25,11 +35,16 @@ get_db
 def get_data
   table = $table
   #sql = "select rowid,* from #{table}"
-  sql = "select rowid,title, directed_by, year, won as Nomin , nom as Won from #{table}"
-  $columns, *rows = $db.execute2(sql)
-  content = rows
+  sql = "select rowid,title, directed_by, year, won , nom , best_pic as bp, best_director as bd, best_actor as ba, best_cinemato as bc, best_foreign as bf, palme, goldenbear as gb, goldenlion as gl from #{table} order by title"
+  # i am moving away from execute2 since it returns ArraywithTypeAndFields which screws up
+  # soeting and searching
+  #$columns, *rows = $db.execute2(sql)
+  q = $db.query(sql + " limit 1 ")
+  $columns = q.columns
+  content = $db.execute(sql)
+  #content = rows
   return nil if content.nil? || content[0].nil?
-  $datatypes = content[0].types #if @datatypes.nil?
+  #$datatypes = content[0].types #if @datatypes.nil?
   return content
 end
 def get_full_row tw
@@ -48,17 +63,59 @@ def view_row tw
   rowid = row[0]
   c, r = get_full_row tw
   list = []
-  r.each_with_index do |r, i|
-    list << c[i]
-    list << r
+  r[0].each_with_index do |rr, i|
+    list << "%20s : %s " % [ c[i], rr ]
   end
   padview list
+end
+def view_wiki tw
+  row = tw.current_value
+  rowid = row[0]
+  url = $db.get_first_value "select url from #{$table} where rowid = #{rowid}"
+  wiki = $db.get_first_value "select wiki from movie_wiki where url = '#{url}'"
+  File.open("tmp.html","w") { |f| f.write(wiki) }
+  text = `w3m -dump -T 'text/html' tmp.html `
+  padview text.split "\n"
 end
 
 def edit_row tw
   row = tw.current_value
+  rowid = row[0]
+  h   = %w{ title directed_by year nom won }
+  c = h.join ","
+  dbrow = $db.get_first_row "select #{c} from #{$table} where rowid = #{rowid}"
+  buffer = []
+  dbrow.each_with_index do |v, ix|
+    buffer << v
+  end
+  _edit h, dbrow, " Edit "
+  dbrow.each_with_index do |v, ix|
+    oldval = buffer[ix]
+    if v != oldval
+      if oldval.nil? && v == ""
+      else
+        alert "modified #{ix} (#{buffer[ix]}) to (#{v}) "
+        $db.execute("update #{$table} set #{h[ix]} = ? where rowid = ? ", [v, rowid])
+      end
+    end
+  end
+
+  # if we try to save back to database at this point we don't know what fields
+  # were actually changed, we could replace a nil with a space since that was done
+  # for populating Field
+  # Also. column names have been changed, so will not be able to update
+  #
+  # How do i change back the original row, unless we query for it and then ...
+  tw.fire_row_changed tw.current_index
+end
+def OLDedit_row tw
+  row = tw.current_value
   h   = tw.columns
   _edit h, row, " Edit "
+  # if we try to save back to database at this point we don't know what fields
+  # were actually changed, we could replace a nil with a space since that was done
+  # for populating Field
+  # Also. column names have been changed, so will not be able to update
   tw.fire_row_changed tw.current_index
 end
 def insert_row tw
@@ -131,7 +188,7 @@ begin
       "#[bg=236, fg=black]#{_col}F1#[/end] Help | #{_col}?#[/end] Keys | #{_col}M-c#[/end] Ask | #{_col}M-d#[/end] History | #{_col}M-m#[/end] Methods | %20s" % [$message.value]
     }
 
-    h = FFI::NCurses.LINES-4
+    h = FFI::NCurses.LINES-2
     w = FFI::NCurses.COLS
     r = 1
     #header = %w[ Pos Last Title Director Year Country Mins BW]
@@ -157,6 +214,31 @@ begin
     end
     mcr = RubyCurses::DefaultTableRenderer.new tv
     mcr.header_colors :white, :red
+
+    # override render_data so we can display certain lines in bold
+    # or with a colored background
+    def mcr.render_data pad, lineno, data
+      text = data.join
+      cp = @color_pair
+      att = @attrib
+      # movies that have won best picture
+      if data[BESTPIC].strip() == "W"
+        att = BOLD
+      end
+      # movies that have won best picture
+      if data[BESTPIC].strip() == "W"
+        cp = get_color($datacolor, :white, :blue)
+      elsif data[BESTPIC].strip() == "N"
+        # movies that have been nominated for best picture
+        cp = get_color($datacolor, :white, 236)
+      elsif data[BESTFOR].strip() == "W" || data[CANNES].strip() == "W"
+        # movies that have won best foreign film or Cannes (palme d or)
+        cp = get_color($datacolor, :white, 234)
+      end
+      FFI::NCurses.wattron(pad,FFI::NCurses.COLOR_PAIR(cp) | att)
+      FFI::NCurses.mvwaddstr(pad, lineno, 0, text)
+      FFI::NCurses.wattroff(pad,FFI::NCurses.COLOR_PAIR(cp) | att)
+    end
     tv.renderer mcr
     mcr.column_model ( tv.column_model )
     tv.create_default_sorter
@@ -173,6 +255,7 @@ begin
       end
     }
     tv.bind_key(?v) { view_row(tv) }
+    tv.bind_key(?x) { view_wiki(tv) }
     tv.bind_key(?e) { edit_row(tv) }
     tv.bind_key(?i) { insert_row(tv) }
     tv.bind_key(?D) { tv.delete_at tv.current_index }
